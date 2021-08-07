00a69fc4c36b98203f0e5ce6fea4a8457357fb18
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-08-07 15:00:39 -0400

    Finish sidebar

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,10 +1,9 @@
-import React from 'react'
-import styled from 'styled-components'
-import { NavLink } from 'react-router-dom'
-import avatar from '../img/avatar.jpg'
+import React from 'react';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import avatar from '../img/avatar.jpg';
 
 const NavigationStyled = styled.nav`
-
   display: flex;
   justify-content: space-between;
   flex-direction: column;
@@ -29,10 +28,37 @@ const NavigationStyled = styled.nav`
   .nav-items {
     width: 100%;
     text-align: center;
+    .active-class {
+      background-color: var(--primary-color);
+    }
     li {
       display: block;
+      padding: 0.2rem 0;
+      position: relative;
+
       a {
         display: block;
+        z-index: 10;
+        &:hover {
+          cursor: pointer;
+        }
+        &::before {
+          content: '';
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          width: 0;
+          height: 50%;
+          background-color: var(--primary-color);
+          transition: All 0.4s cubic-bezier(1, -0.19, 0.31, 0.94);
+          z-index: 3;
+          opacity: 0.21;
+        }
+      }
+
+      a:hover::before {
+        width: 100%;
+        height: 100%;
       }
     }
   }
@@ -47,39 +73,51 @@ const NavigationStyled = styled.nav`
       text-align: center;
     }
   }
-`
+`;
 
 const Navigation = () => {
   return (
     <NavigationStyled>
-      <div className="avatar">
-        <img src={avatar} alt="" />
+      <div className='avatar'>
+        <img src={avatar} alt='' />
       </div>
       <ul className='nav-items'>
-        <li className="nav-item">
-          <NavLink to='/home' activeClassName='active'>Home</NavLink>
+        <li className='nav-item'>
+          <NavLink to='/home' activeClassName='active-class'>
+            Home
+          </NavLink>
         </li>
-        <li className="nav-item">
-          <NavLink to='/about' activeClassName='active'>About</NavLink>
+        <li className='nav-item'>
+          <NavLink to='/about' activeClassName='active-class'>
+            About
+          </NavLink>
         </li>
-        <li className="nav-item">
-          <NavLink to='/resume' activeClassName='active'>Resume</NavLink>
+        <li className='nav-item'>
+          <NavLink to='/resume' activeClassName='active-class'>
+            Resume
+          </NavLink>
         </li>
-        <li className="nav-item">
-          <NavLink to='/portfolios' activeClassName='active'>Portfolios</NavLink>
+        <li className='nav-item'>
+          <NavLink to='/portfolios' activeClassName='active-class'>
+            Portfolios
+          </NavLink>
         </li>
-        <li className="nav-item">
-          <NavLink to='/blogs' activeClassName='active'>Blogs</NavLink>
+        <li className='nav-item'>
+          <NavLink to='/blogs' activeClassName='active-class'>
+            Blogs
+          </NavLink>
         </li>
-        <li className="nav-item">
-          <NavLink to='/contact' activeClassName='active'>Contact</NavLink>
+        <li className='nav-item'>
+          <NavLink to='/contact' activeClassName='active-class'>
+            Contact
+          </NavLink>
         </li>
       </ul>
-      <footer className="footer">
+      <footer className='footer'>
         <p>@2021 My Website</p>
       </footer>
     </NavigationStyled>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
