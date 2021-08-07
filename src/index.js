72bde9655b0d7181beae464c21fe65fb202898b0
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-08-07 14:23:15 -0400

    Comment out unused imports

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import GlobalStyle from './styles/GlobalStyle';
-import Theme from './theme/theme';
-import { ThemeProvider } from 'styled-components';
+// import Theme from './theme/theme';
+// import { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = document.getElementById('root');
