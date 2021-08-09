e1f877d325daf89407162d2fd741e3df97d88646
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-08-08 22:17:49 -0400

    BlogsPage and ContactPage

--- src/pages/BlogsPage.js
+++ src/pages/BlogsPage.js
@@ -1,11 +1,70 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Title from '../Components/Title';
+import blogs from '../data/blogs';
+import { MainLayout, InnerLayout } from '../styles/Layout';
+
+const BlogsStyled = styled.div`
+  .blog {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-column-gap: 2rem;
+    grid-row-gap: 3rem;
+    .blog-item {
+      padding: 2rem 1rem;
+      background-color: var(--background-dark-grey);
+    }
+    .image {
+      overflow: hidden;
+      width: 100%;
+
+      img {
+        width: 100%;
+        height: 85%;
+        object-fit: cover;
+        transition: all .2s ease-in-out;
+        &:hover {
+          cursor: pointer;
+          transform: rotate(-3deg) scale(1.1);
+        }
+      }
+    }
+    .title {
+      a {
+        font-size: 1.5rem;
+        color: var(--color-white);
+        cursor: pointer;
+        transition: all .2s ease-in-out;
+        &:hover {
+          color: var(--primary-color);
+        }
+      }
+    }
+  }
+`;
 
 const BlogsPage = () => {
   return (
-    <div>
-      Blogs
-    </div>
-  )
-}
+    <MainLayout>
+      <BlogsStyled>
+        <Title title={'Blogs'} span={'Blogs'} />
+        <InnerLayout className={'blog'}>
+          {blogs.map((blog) => {
+            return (
+              <div key={blog.id} className={'blog-item'}>
+                <div className='image'>
+                  <img src={blog.image} alt='' />
+                </div>
+                <div className='title'>
+                  <a href={blog.link}>{blog.title}</a>
+                </div>
+              </div>
+            );
+          })}
+        </InnerLayout>
+      </BlogsStyled>
+    </MainLayout>
+  );
+};
 
-export default BlogsPage
+export default BlogsPage;
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,11 +1,130 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { MainLayout, InnerLayout } from '../styles/Layout';
+import Title from '../Components/Title';
+import PrimaryButton from '../Components/PrimaryButton';
+import PhoneIcon from '@material-ui/icons/Phone';
+import EmailIcon from '@material-ui/icons/Email';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import ContactItem from '../Components/ContactItem';
+
+const ContactPageStyled = styled.section`
+  .contact-section {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-column-gap: 2rem;
+    .right-content {
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+    }
+    .contact-title {
+      h4 {
+        color: var(--white-color);
+        font-size: 1.8rem;
+      }
+    }
+    .form {
+      width: 100%;
+      .form-field {
+        margin-top: 2rem;
+        position: relative;
+        width: 100%;
+        label {
+          position: absolute;
+          left: 20px;
+          top: -18px;
+          display: inline-block;
+          background-color: var(--background-dark-color);
+          padding: 0 0.5rem;
+          color: inherit;
+        }
+        input {
+          border: 1px solid var(--border-color);
+          outline: none;
+          background-color: transparent;
+          height: 70px;
+          padding: 0.5rem 15px;
+          width: 100%;
+          color: inherit;
+        }
+        textarea {
+          background-color: transparent;
+          border: 1px solid var(--border-color);
+          outline: none;
+          color: inherit;
+          width: 100%;
+          padding: 0.8rem;
+        }
+      }
+    }
+  }
+`;
 
 const ContactPage = () => {
+  const phone = <PhoneIcon />;
+  const email = <EmailIcon />;
+  const location = <LocationOnIcon />;
   return (
-    <div>
-      ContactPage
-    </div>
-  )
-}
+    <MainLayout>
+      <ContactPageStyled>
+        <Title title={'Contact'} span={'Contact'} />
+        <InnerLayout className={'contact-section'}>
+          <div className='left-content'>
+            <div className='contact-title'>
+              <h4>Get In Touch</h4>
+            </div>
+            <form action='' className='form'>
+              <div className='form-field'>
+                <label htmlFor='name'>Enter your name</label>
+                <input type='text' name='name' id='name' />
+              </div>
+              <div className='form-field'>
+                <label htmlFor='email'>Enter your email*</label>
+                <input type='email' id='email' />
+              </div>
+              <div className='form-field'>
+                <label htmlFor='subject'>Enter your subject</label>
+                <input type='text' id='subject' />
+              </div>
+              <div className='form-field'>
+                <label htmlFor='text-area'>Enter your Message*</label>
+                <textarea
+                  name='textarea'
+                  id='textarea'
+                  cols='30'
+                  rows='10'
+                ></textarea>
+              </div>
+              <div className='form-field f-button'>
+                <PrimaryButton title={'Send Email'} />
+              </div>
+            </form>
+          </div>
+          <div className='right-content'>
+            <ContactItem
+              title={'Phone'}
+              icon={phone}
+              cont1={'+66-789675637'}
+              cont2={'07663520283'}
+            />
+            <ContactItem
+              title={'Email'}
+              icon={email}
+              cont1={'loremipsum@gmail.com'}
+              cont2={'info.lorem.ipsum@gmail.com'}
+            />
+            <ContactItem
+              title={'Address'}
+              icon={location}
+              cont1={'27 Aldrich Road, London, England'}
+              cont2={'United Kingdom'}
+            />
+          </div>
+        </InnerLayout>
+      </ContactPageStyled>
+    </MainLayout>
+  );
+};
 
-export default ContactPage
+export default ContactPage;
